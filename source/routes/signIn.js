b08cbb838e878874f9f9a730e4604773c2f8b245
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2020-12-09 17:49:01 +0530

    returning id with signin

--- source/routes/signIn.js
+++ source/routes/signIn.js
@@ -6,26 +6,31 @@ const router = express.Router()
 
 // takes valid user email and password and returns user a token
 router.post('/', async (req, res) => {
-  const { error } = validateUser(req.body)
-  if (error) return res.status(400).send('Wrong Credentials!')
+	const { error } = validateUser(req.body)
+	if (error) return res.status(400).send('Wrong Credentials!')
 
-  const user = await User.findOne({ email: req.body.email })
-  if (!user) return res.status(404).send('User not found')
+	const user = await User.findOne({ email: req.body.email })
+	if (!user) return res.status(404).send('User not found')
 
-  if (bcrypt.compare(req.body.password, user.password)) {
-    const token = user.generateAuthToken()
-    return res.send({ token, callsign: user.callsign, avatar: user.avatar })
-  }
+	if (bcrypt.compare(req.body.password, user.password)) {
+		const token = user.generateAuthToken()
+		return res.send({
+			_id: user._id,
+			token,
+			callsign: user.callsign,
+			avatar: user.avatar,
+		})
+	}
 
-  return res.status(400).send('Invalid Authentication!')
+	return res.status(400).send('Invalid Authentication!')
 })
 
 function validateUser(user) {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  })
-  return schema.validate(user)
+	const schema = Joi.object({
+		email: Joi.string().email().required(),
+		password: Joi.string().required(),
+	})
+	return schema.validate(user)
 }
 
 module.exports = router
